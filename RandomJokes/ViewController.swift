eb1fe1705177b4c800fb3a3151d68bc9612bdd60
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2019-09-20 19:22:13 +0200

    Initial Commit

---
+++ RandomJokes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomJokes
+//
+//  Created by Francesco Chiusolo on 20/09/2019.
+//  Copyright © 2019 Francesco Chiusolo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
