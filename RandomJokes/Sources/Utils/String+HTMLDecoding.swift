981831911f92c41c8f907856a35f15e38973537b
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2019-10-23 15:28:44 +0200

    Refactor decoding function

--- RandomJokes/Sources/Utils/String+HTMLDecoding.swift
+++ RandomJokes/Sources/Utils/String+HTMLDecoding.swift
@@ -1,20 +1,22 @@
 import UIKit
 
 extension String {
+    private static let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+        .documentType: NSAttributedString.DocumentType.html,
+        .characterEncoding: String.Encoding.utf8.rawValue
+    ]
+    
     init?(htmlEncodedString: String) {
-        guard let data = htmlEncodedString.data(using: .utf8) else {
+        guard let attributedString = String.buildAttributedString(htmlEncodedString) else {
             return nil
         }
-
-        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
-            .documentType: NSAttributedString.DocumentType.html,
-            .characterEncoding: String.Encoding.utf8.rawValue
-        ]
-
-        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
-            return nil
-        }
-
+        
         self.init(attributedString.string)
+    }
+    
+    private static func buildAttributedString(_ htmlEncodedString: String) -> NSAttributedString? {
+        return htmlEncodedString
+            .data(using: .utf8)
+            .flatMap { try? NSAttributedString(data: $0, options: String.options, documentAttributes: nil) }
     }
 }
