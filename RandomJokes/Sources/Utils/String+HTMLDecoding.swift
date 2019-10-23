3696f20e14ae9de14a463654f62dfd55b201f327
Francesco Chiusolo <chiusolofrancesco@gmail.com>
2019-10-23 15:17:42 +0200

    Decode html encoded strings

---
+++ RandomJokes/Sources/Utils/String+HTMLDecoding.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+extension String {
+    init?(htmlEncodedString: String) {
+        guard let data = htmlEncodedString.data(using: .utf8) else {
+            return nil
+        }
+
+        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
+            .documentType: NSAttributedString.DocumentType.html,
+            .characterEncoding: String.Encoding.utf8.rawValue
+        ]
+
+        guard let attributedString = try? NSAttributedString(data: data, options: options, documentAttributes: nil) else {
+            return nil
+        }
+
+        self.init(attributedString.string)
+    }
+}
